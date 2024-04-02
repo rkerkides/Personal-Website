62d934f12638ee8c6ca3e17f5a4fb26de745b3f7
Renos Kerkides <rkerkides@gmail.com>
2024-04-02 23:55:12 +0100

    optimize TypingEffect visibility with IntersectionObserver

--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Typewriter from "typewriter-effect";
 
 function TypingEffect() {
+  const [isVisible, setIsVisible] = useState(false);
+  const typingEffectRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        threshold: 0.1,
+      }
+    );
+
+    if (typingEffectRef.current) {
+      observer.observe(typingEffectRef.current);
+    }
+
+    return () => {
+      if (typingEffectRef && typingEffectRef.current) {
+        observer.unobserve(typingEffectRef.current);
+      }
+    };
+  }, [typingEffectRef]);
+
+
   return (
-    <div className="typewriter-container">
-      <Typewriter
-        options={{
-          strings: [
-            "Inspiring",
-            "Aspiring<span style='color: white;'>*</span> software dev",
-            "From Nicosia, Cyprus",
-            "Software Development MSc",
-            "University of Glasgow",
-          ],
-          autoStart: true,
-          loop: true,
-          delay: 50,
-          deleteSpeed: 20,
-        }}
-      />
+    <div ref={typingEffectRef} className="typewriter-container">
+      {isVisible && (
+        <Typewriter
+          options={{
+            strings: [
+              "Inspiring",
+              "Aspiring<span style='color: #FFF;'>*</span> software dev",
+              "From Nicosia, Cyprus",
+              "Software Development MSc",
+              "University of Glasgow",
+            ],
+            autoStart: true,
+            loop: true,
+            delay: 50,
+            deleteSpeed: 20,
+          }}
+        />
+      )}
     </div>
   );
 }
 
 export default TypingEffect;
+
