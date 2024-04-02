2c3ba8d1020756d79d04c895d1da923d77b96cb9
Renos Kerkides <rkerkides@gmail.com>
2024-04-02 19:57:41 +0100

    improve SEO

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
